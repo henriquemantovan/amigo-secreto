46eb76f46560b7d1997b80d50a442d8c27f6c2f4
Henrique <henrique.mantovan@usp.br>
2025-08-05 05:14:41 -0300

    add amigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeNomes = [];
+
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo');
+    if (nome.value == '') {
+        alert('Por favor, insira um nome.');
+    } else if (listaDeNomes.includes(nome.value)){
+        alert('Esse nome já está na lista');
+    } else {
+        listaDeNomes.push(nome.value);
+    }
+    nome.value = '';
+    listarAmigos();
+}
+
+
